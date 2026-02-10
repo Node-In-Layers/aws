f5f71e6d4d85da23683c431101543540e221f666
Mike Cornwell <mike@mikecornwell.com>
2026-02-10 15:40:57 -0500

    feat(index): update index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+// Export Types
+export * from './types.js'
+export * from './config/types.js'
+
+// Export default domain
 export * from './aws/index.js'
+
+// Export named domains
 export * as aws from './aws/index.js'
 export * as config from './config/index.js'
